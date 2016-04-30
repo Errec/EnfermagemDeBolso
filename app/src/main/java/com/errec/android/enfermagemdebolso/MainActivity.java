22ccb743755bb24fdc9f099d57236fa6934c7038
Errec <ranirocoelho@gmail.com>
2016-04-30 18:38:13 -0300

    liking activities part 1

--- app/src/main/java/com/errec/android/enfermagemdebolso/MainActivity.java
+++ app/src/main/java/com/errec/android/enfermagemdebolso/MainActivity.java
@@ -1,7 +1,8 @@
 package com.errec.android.enfermagemdebolso;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setupDropCalcButton();
+        setupDictionaryButton();
     }
+
+    private void setupDropCalcButton() {
+
+        startActivity(new Intent(MainActivity.this, DropCalc.class));
+    }
+
+    private void setupDictionaryButton() {
+
+        startActivity(new Intent(MainActivity.this, MedicalDictionary.class));
+    }
+
+
 }
