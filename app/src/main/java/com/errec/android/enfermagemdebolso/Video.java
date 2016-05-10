567d99c8d4b84ccbf97eb0228c983fe3dc289879
Errec <ranirocoelho@gmail.com>
2016-05-09 21:54:42 -0300

    Youtube activity: part 4

---
+++ app/src/main/java/com/errec/android/enfermagemdebolso/Video.java
@@ -0,0 +1,23 @@
+package com.errec.android.enfermagemdebolso;
+
+/**
+ * Created by ${Raniro} on ${29/04/2016}.
+ */
+public class Video {
+
+    private String id;
+    private String title;
+
+    public Video(String id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
