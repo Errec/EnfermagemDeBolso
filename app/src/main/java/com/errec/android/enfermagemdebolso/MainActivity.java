0cb50fd8d7fbacb2b80e42389812fcebf3f3e81f
Errec <ranirocoelho@gmail.com>
2016-05-18 22:31:45 -0300

    Youtube activity: part 12

--- app/src/main/java/com/errec/android/enfermagemdebolso/MainActivity.java
+++ app/src/main/java/com/errec/android/enfermagemdebolso/MainActivity.java
@@ -6,12 +6,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_main);
 
         setupDropCalcButton();
