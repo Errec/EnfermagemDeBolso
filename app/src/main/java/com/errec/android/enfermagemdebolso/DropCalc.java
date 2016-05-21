64f9f618de97d031dbb4987b2e33f2baffc08650
Errec <ranirocoelho@gmail.com>
2016-05-20 21:35:14 -0300

    setup drop calculator logic part 1

--- app/src/main/java/com/errec/android/enfermagemdebolso/DropCalc.java
+++ app/src/main/java/com/errec/android/enfermagemdebolso/DropCalc.java
@@ -1,13 +1,48 @@
 package com.errec.android.enfermagemdebolso;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.InputType;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class DropCalc extends AppCompatActivity {
+
+    EditText etVolInput;
+    EditText etTimeInput;
+    EditText focusedView;
+    private View.OnFocusChangeListener focusListener = new View.OnFocusChangeListener(){
+
+        public void onFocusChange(View v, boolean hasFocus) {
+            if (hasFocus){
+                focusedView = (EditText)v;
+            } else {
+                focusedView  = null;
+            }
+        }
+
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drop_calc);
+
+        etVolInput = (EditText) findViewById(R.id.edittext_volume);
+        etTimeInput = (EditText) findViewById(R.id.edittext_time);
+
+        ((EditText) findViewById(R.id.edittext_time)).setInputType(InputType.TYPE_NULL);
+        ((EditText) findViewById(R.id.edittext_volume)).setInputType(InputType.TYPE_NULL);
+
+        etVolInput.setOnFocusChangeListener(focusListener);
+        etTimeInput.setOnFocusChangeListener(focusListener);
+
     }
+
+    public void numClicked(View sender){
+        Button bt = (Button) sender;
+        focusedView.append(bt.getText());
+    }
+
 }
