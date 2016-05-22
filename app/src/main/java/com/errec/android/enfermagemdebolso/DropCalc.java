52894139ad15011eee75788f8ca9a5d523abc412
Errec <ranirocoelho@gmail.com>
2016-05-21 23:10:55 -0300

    setup drop calculator logic part 2

--- app/src/main/java/com/errec/android/enfermagemdebolso/DropCalc.java
+++ app/src/main/java/com/errec/android/enfermagemdebolso/DropCalc.java
@@ -6,12 +6,15 @@ import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class DropCalc extends AppCompatActivity {
 
     EditText etVolInput;
     EditText etTimeInput;
     EditText focusedView;
+    TextView resultTextView;
+
     private View.OnFocusChangeListener focusListener = new View.OnFocusChangeListener(){
 
         public void onFocusChange(View v, boolean hasFocus) {
@@ -31,6 +34,7 @@ public class DropCalc extends AppCompatActivity {
 
         etVolInput = (EditText) findViewById(R.id.edittext_volume);
         etTimeInput = (EditText) findViewById(R.id.edittext_time);
+        resultTextView = (TextView) findViewById(R.id.text_result);
 
         ((EditText) findViewById(R.id.edittext_time)).setInputType(InputType.TYPE_NULL);
         ((EditText) findViewById(R.id.edittext_volume)).setInputType(InputType.TYPE_NULL);
@@ -42,7 +46,29 @@ public class DropCalc extends AppCompatActivity {
 
     public void numClicked(View sender){
         Button bt = (Button) sender;
+
+        if(focusedView.getText().length() > 4){
+            return;
+        }
+
         focusedView.append(bt.getText());
+    }
+
+    public void resultClicked(View view){
+
+        double volume = Double.parseDouble(etVolInput.getText().toString());
+        double time = Double.parseDouble(etTimeInput.getText().toString());
+
+        double result = time * volume;
+
+        resultTextView.setText(String.valueOf(result));
+    }
+
+    public void resetClicked(View view){
+
+        resultTextView.setText("");
+        etTimeInput.setText("");
+        etVolInput.setText("");
     }
 
 }
